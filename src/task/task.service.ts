5492a3f96c1b04f2228c74559816a210194e89bc
josusk8 <josusk8@gmail.com>
2022-08-14 16:49:12 +0100

    CRUD Task, @Update - complete

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -25,19 +26,27 @@ export class TaskService {
   })
   }
 
-  create(body: any) {
+  create( { user, tittle, priority, description }: Task) {
     const newTask = new Task();
-    newTask.tittle = body.tittle;
+    newTask.tittle = tittle;
+    newTask.description= description;
+    newTask.priority= priority;
+    newTask.user = user
+
     // const newTask = this.tasksRepo.create(body);
     return this.taskRepo.save(newTask);
   }
-/*
+
   async update(id: number, body: any) {
-    const task = await this.taskRepo.findOne(id);
+    const task = await this.taskRepo.findOne({
+      where: {
+          id: id
+      }
+  })
     this.taskRepo.merge(task, body);
     return this.taskRepo.save(task);
   }
-*/
+
   async remove(id: number) {
     await this.taskRepo.delete(id);
     return true;
